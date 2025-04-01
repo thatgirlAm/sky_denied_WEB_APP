5e6108d52b60a9ee097c015adab195ef38aa87ba
ThatgirlAm <atm_diop@icloud.com>
2025-04-01 02:56:25 +0100

    fix: adding the HasApiTokens trait to flight_info model

--- back/app/Models/Flight_info.php
+++ back/app/Models/Flight_info.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Flight_info extends Model
 {
-    use Notifiable;
+    use Notifiable, HasApiTokens;
 
     protected $fillable=[
         
