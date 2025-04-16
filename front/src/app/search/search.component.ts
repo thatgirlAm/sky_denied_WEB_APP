6ae493fcd48d59d7872bc5dae2877474d4be86e9
charlottedvl <charlotte.deville@eleve.isep.fr>
2025-04-16 19:05:07 +0100

    dev:create prediction result component

--- front/src/app/search/search.component.ts
+++ front/src/app/search/search.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchFormComponent } from '../search-form/search-form.component';
 import { SearchResultsComponent } from '../search-results/search-results.component';
+import { PredictionResultsComponent } from '../prediction-results/prediction-results.component';
 
 @Component({
   selector: 'app-search',
-  imports: [SearchFormComponent, SearchResultsComponent],
+  imports: [SearchFormComponent, SearchResultsComponent, PredictionResultsComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
