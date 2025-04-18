6fa0be4c95834d1f31db8f058316a39f10f8d6e8
charlottedvl <charlotte.deville@eleve.isep.fr>
2025-04-18 18:51:09 +0100

    dev:Add homepage without logic

--- front/src/app/homepage/homepage.component.ts
+++ front/src/app/homepage/homepage.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
+  activeTab: string = 'have-flight'; // Set the initial active tab
 
+  switchTab(tabId: string) {
+    this.activeTab = tabId;
+  }
 }
