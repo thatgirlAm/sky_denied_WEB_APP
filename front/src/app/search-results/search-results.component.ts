c5bbe771bbb469cb56dadb10ed157f37d44b7389
charlottedvl <charlotte.deville@eleve.isep.fr>
2025-04-18 20:24:40 +0100

    dev:Add loaders to search results and prediction pop up

--- front/src/app/search-results/search-results.component.ts
+++ front/src/app/search-results/search-results.component.ts
@@ -1,12 +1,15 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-search-results',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './search-results.component.html',
   styleUrl: './search-results.component.css'
 })
 export class SearchResultsComponent {
+  loaded: boolean = false;
+
   @Output() predictClicked = new EventEmitter<void>();
   @Output() notifyClicked = new EventEmitter<void>();
 
