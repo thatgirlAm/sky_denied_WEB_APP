45899934225a00de2bd90273e8956f0d05ffa4bc
ThatgirlAm <atm_diop@icloud.com>
2025-04-19 18:44:51 +0100

    dev: the rest

--- front/src/app/homepage/homepage.component.ts
+++ front/src/app/homepage/homepage.component.ts
@@ -2,18 +2,20 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ApiService } from '../api-service.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { log } from 'node:console';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [RouterModule, ReactiveFormsModule], 
+  imports: [RouterModule, ReactiveFormsModule, NgIf], 
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
   activeTab = 'have-flight';
   form: FormGroup;
-
+  isLoading = false;
   constructor(private api: ApiService, private fb: FormBuilder) {
     this.form = this.fb.group({
       tail_number: ['', Validators.required],
@@ -24,17 +26,23 @@ export class HomepageComponent {
     });
   }
 
-  fetchFlightData(): void {
+  async fetchFlightData() {
+    console.log("button clicked");
+    
     if (this.form.invalid) return;
 
     const tailNumber = this.form.value.tailNumber;
+
     this.api.searchFlights(tailNumber).subscribe({
       next: (flights) => {
+        this.isLoading = true ; 
         console.log('Flight Data:', flights);
         console.table(flights);
       },
-      error: (err) => console.error('Error:', err)
-    });
+      error: (err) => {
+        console.error('Error:', err);
+        this.isLoading = false;
+  }});
   }
 
   switchTab(tabId: string): void {
