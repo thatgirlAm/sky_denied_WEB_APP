f53789d8b6fbd1efbabbf7aaaec84e2c42d5de04
charlottedvl <charlotte.deville@eleve.isep.fr>
2025-04-21 16:28:38 +0100

    dev:create contact form component

--- front/src/app/about-us/about-us.component.ts
+++ front/src/app/about-us/about-us.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-about-us',
-  imports: [RouterModule],
+  imports: [RouterModule, ContactComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
--- front/src/app/contact/contact.component.ts
+++ front/src/app/contact/contact.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {Flight} from '../flight';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  isLoading = false;
+  submitted = false;
+  contactForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.contactForm = this.fb.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      email: ['', Validators.required],
+      topic: ['', Validators.required],
+      body: ['', Validators.required],
+    });
+  }
+
+
+  contactFormSubmit() {
+    this.submitted = true;
+
+    this.contactForm.markAllAsTouched();
+    if (this.contactForm.invalid) return;
+
+    // Build search parameters based on active tab
+    const searchParams =
+    {
+      first_name: this.contactForm.value.first_name,
+      last_name: this.contactForm.value.last_name,
+      email: this.contactForm.value.email,
+      topic: this.contactForm.value.topic,
+      body: this.contactForm.value.body
+    };
+    console.log(searchParams);
+
+    //Sending message to sky denied email
+  }
 
 }
