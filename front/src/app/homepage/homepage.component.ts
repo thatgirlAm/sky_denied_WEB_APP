a401e2e3e3779306b8a82eed5eba087a59fb08df
ThatgirlAm <atm_diop@icloud.com>
2025-04-30 03:21:19 +0100

    fix: the homepage and everything is functional

--- front/src/app/homepage/homepage.component.ts
+++ front/src/app/homepage/homepage.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import {Flight} from '../flight';
 import { SearchFormComponent} from '../search-form/search-form.component';
 import { ApiService } from '../api-service.service';
 import { ToastrService } from 'ngx-toastr';
+import { SearchResultsComponent } from '../search-results/search-results.component';
+import { PredictionResultsComponent } from '../prediction-results/prediction-results.component';
+import { InputEmailFormComponent } from '../input-email-form/input-email-form.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [RouterModule, ReactiveFormsModule, SearchFormComponent],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule, SearchResultsComponent, PredictionResultsComponent,InputEmailFormComponent, SearchFormComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
@@ -18,14 +21,26 @@ export class HomepageComponent {
 
   activeTab: string = 'have-flight';
   loader: boolean = false;
-  flights : Flight[] = []; 
+  //flights : Flight[] = []; 
   showTable : boolean = false ; 
+  @Input() flights: Flight[] = [];
+  @Output() predictClicked = new EventEmitter<Flight>();
+  @Output() notifyClicked = new EventEmitter<Flight>();
+  showPredictionPopup = false;
+  showGetNotifiedPopup = false;
+  searchClicked = false ;
+  searchResults: Flight[] = this.flights;
+  searchForm !: FormGroup ; 
+
+  // to know if the model has been triggered
+  isModelTriggering = false;
+  previousSearch = false ; 
+  searchParams !: any ; 
+  isLoading = false;
+  submitted = false;
+
 
   constructor(private fb: FormBuilder, private apiService: ApiService, private toastrService: ToastrService) {}
-
-
-  searchClicked = false ;
-  searchResults: Flight[] = [];
 
   handleSearchStarted() {
   this.searchClicked = true;
@@ -36,5 +51,29 @@ export class HomepageComponent {
   this.searchResults = flights;
   this.searchClicked = true;
   }
+
+
+  handlePredictionTrigger() {
+    this.searchClicked = true ;
+    this.showPredictionPopup = true;
+  }
+
+  closePredictionPopup() {
+    this.showPredictionPopup = false;
+  }
+
+  handleGetNotifiedTrigger() {
+    console.log('Get Notified Triggered');
+
+    this.showGetNotifiedPopup = true;
+  }
+
+  closeGetNotifiedPopup() {
+    console.log('Get Notified closed');
+
+    this.showGetNotifiedPopup = false;
+  }
+
+
 
 }
